40ac7cdb8b4218eaa0b09029e66531e0d40b10bd
John Kline <jtkline1205@gmail.com>
2023-12-11 22:48:41 -0500

    updates

--- app.py
+++ app.py
@@ -17,110 +17,115 @@ TWENTY_FIVE = 25.0
 FIFTY = 50.0
 HUNDRED = 100.0
 
+the_map = {
+    ONE: {
+        "color": "white",
+        "dollar_jpeg": "one_dollar_bill.jpg",
+        "chip_change": "1 White",
+        "bill_to_chip": "ONE",
+        "cash_exchange": "one_to_white.png",
+        "chip_exchange": "white_to_one.png",
+    },
+    TWO_FIFTY: {
+        "color": "blue",
+        "dollar_jpeg": "",
+        "chip_change": "",
+        "bill_to_chip": "",
+        "cash_exchange": "",
+        "chip_exchange": "",
+    },
+    FIVE: {
+        "color": "red",
+        "dollar_jpeg": "five_dollar_bill.png",
+        "chip_change": "1 Red",
+        "bill_to_chip": "FIVE",
+        "cash_exchange": "five_to_red.png",
+        "chip_exchange": "red_to_five.png",
+    },
+    TEN: {
+        "dollar_jpeg": "ten_dollar_bill.jpg",
+        "chip_change": "2 Red",
+        "bill_to_chip": "FIVE",
+        "cash_exchange": "ten_to_red.png",
+        "chip_exchange": "red_to_ten.png",
+    },
+    TWENTY: {
+        "dollar_jpeg": "twenty_dollar_bill.jpg",
+        "chip_change": "4 Red",
+        "bill_to_chip": "FIVE",
+        "cash_exchange": "twenty_to_red.png",
+        "chip_exchange": "red_to_twenty.png",
+    },
+    TWENTY_FIVE: {
+        "color": "green",
+        "dollar_jpeg": "",
+        "chip_change": "",
+        "bill_to_chip": "",
+        "cash_exchange": "",
+        "chip_exchange": "",
+    },
+    FIFTY: {
+        "dollar_jpeg": "fifty_dollar_bill.jpg",
+        "chip_change": "2 Green",
+        "bill_to_chip": "TWENTY_FIVE",
+        "cash_exchange": "fifty_to_green.png",
+        "chip_exchange": "green_to_fifty.png",
+    },
+    HUNDRED: {
+        "color": "black",
+        "dollar_jpeg": "hundred_dollar_bill.jpg",
+        "chip_change": "1 Black",
+        "bill_to_chip": "HUNDRED",
+        "cash_exchange": "hundred_to_black.png",
+        "chip_exchange": "black_to_hundred.png",
+    },
+}
+
 
 @app.route('/colors/chips', methods=['GET'])
 def chip_color():
-    resource_name_map = {
-        ONE: "white",
-        TWO_FIFTY: "blue",
-        FIVE: "red",
-        TWENTY_FIVE: "green",
-        HUNDRED: "black",
-    }
     try:
-        double_param = float(request.args.get('double'))
-        resource_name = resource_name_map[double_param]
-        return jsonify({"color": resource_name})
+        return jsonify({"color": the_map[float(request.args.get('double'))]["color"]})
     except (ValueError, KeyError):
         return jsonify({"error": "Invalid chip value"}), 400
 
 
 @app.route('/images/bills', methods=['GET'])
 def dollar_jpeg_name():
-    resource_name_map = {
-        ONE: "one_dollar_bill.jpg",
-        FIVE: "five_dollar_bill.png",
-        TEN: "ten_dollar_bill.jpg",
-        TWENTY: "twenty_dollar_bill.jpg",
-        FIFTY: "fifty_dollar_bill.jpg",
-        HUNDRED: "hundred_dollar_bill.jpg"
-    }
     try:
-        bill_denomination = float(request.args.get('double'))
-        resource_name = resource_name_map[bill_denomination]
-        return jsonify({"dollar_jpeg_name": resource_name})
+        return jsonify({"dollar_jpeg_name": the_map[float(request.args.get('double'))]['dollar_jpeg']})
     except (ValueError, KeyError):
         return jsonify({"error": "Invalid bill value"}), 400
 
 
 @app.route('/strings/chips/change', methods=['GET'])
 def chip_change_string():
-    resource_name_map = {
-        ONE: "1 White",
-        FIVE: "1 Red",
-        TEN: "2 Red",
-        TWENTY: "4 Red",
-        FIFTY: "2 Green",
-        HUNDRED: "1 Black"
-    }
     try:
-        double_value = float(request.args.get('double'))
-        resource_name = resource_name_map[double_value]
-        return jsonify({"chip_change_string": resource_name})
+        return jsonify({"chip_change_string": the_map[float(request.args.get('double'))]['chip_change']})
     except (ValueError, KeyError):
         return jsonify({"error": "Invalid chip value"}), 400
 
 
 @app.route('/strings/bills/chips', methods=['GET'])
 def bill_to_chip_string():
-    resource_name_map = {
-        "ONE": "ONE",
-        "FIVE": "FIVE",
-        "TEN": "FIVE",
-        "TWENTY": "FIVE",
-        "FIFTY": "TWENTY_FIVE",
-        "HUNDRED": "HUNDRED",
-    }
     try:
-        bill_name = str(request.args.get('name'))
-        resource_name = resource_name_map[bill_name]
-        return jsonify({"chip_name": resource_name})
+        return jsonify({"chip_name": the_map[float(request.args.get('billValue'))]['bill_to_chip']})
     except (ValueError, KeyError):
         return jsonify({"error": "Invalid bill name"}), 400
 
 
 @app.route('/images/bills/change', methods=['GET'])
 def cash_exchange_filename():
-    resource_name_map = {
-        "ONE": "one_to_white.png",
-        "FIVE": "five_to_red.png",
-        "TEN": "ten_to_red.png",
-        "TWENTY": "twenty_to_red.png",
-        "FIFTY": "fifty_to_green.png",
-        "HUNDRED": "hundred_to_black.png"
-    }
     try:
-        denomination_name = str(request.args.get('name'))
-        resource_name = resource_name_map[denomination_name]
-        return jsonify({"cash_exchange_filename": resource_name})
+        return jsonify({"cash_exchange_filename": the_map[float(request.args.get('billValue'))]['cash_exchange']})
     except (ValueError, KeyError):
         return jsonify({"error": "Invalid bill value"}), 400
 
 
 @app.route('/images/chips/change', methods=['GET'])
 def chip_exchange_filename():
-    resource_name_map = {
-        "ONE": "white_to_one.png",
-        "FIVE": "red_to_five.png",
-        "TEN": "red_to_ten.png",
-        "TWENTY": "red_to_twenty.png",
-        "FIFTY": "green_to_fifty.png",
-        "HUNDRED": "black_to_hundred.png"
-    }
     try:
-        denomination_name = str(request.args.get('name'))
-        resource_name = resource_name_map[denomination_name]
-        return jsonify({"chip_exchange_filename": resource_name})
+        return jsonify({"chip_exchange_filename": the_map[float(request.args.get('chipValue'))]['chip_exchange']})
     except (ValueError, KeyError):
         return jsonify({"error": "Invalid chip value"}), 400
 
